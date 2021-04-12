ea3aa5ce7cf9cd6a1f06d1bc59d533ebc86564e3
castetus <kboikov@mail.ru>
2021-04-12 13:13:38 +0300

    minesweeper!!!

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,40 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  function checkMines(line, index, lineIndex) {
+    let sum = 0;
+    function test(line2, index2) {
+      let counter = 0;
+      for (let i = index2 - 1; i <= index2 + 1; i++) {
+        if (line2[i]) {
+          counter++;
+        }
+      }
+
+      return counter;
+    }
+    if (matrix[lineIndex - 1]) {
+      sum += test(matrix[lineIndex - 1], index);
+    }
+    if (matrix[lineIndex + 1]) {
+      sum += test(matrix[lineIndex + 1], index);
+    }
+    sum += test(line, index);
+    return sum;
+  }
+
+  const result = matrix.map((line, lineIndex) => line.map((elem, index) => {
+    if (elem === false) {
+      if (lineIndex === 1) {
+        checkMines(line, index, lineIndex);
+      }
+      return checkMines(line, index, lineIndex);
+    }
+    return 1;
+  }));
+
+  return result;
 }
 
 module.exports = minesweeper;
