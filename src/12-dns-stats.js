82b360197ab8dfac2bb69503a4862bc245e81375
castetus <kboikov@mail.ru>
2021-04-12 14:30:49 +0300

    dns & mac

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const result = {};
+
+  const domainsArrs = domains.map((domain) => domain.split('.').reverse());
+
+  // const maxLength = domainsArrs.reduce((acc, item) => {
+  //   let result2;
+  //   if (acc.length <= item.length) {
+  //     result2 = item.length;
+  //   }
+  //   return result2;
+  // });
+
+  domainsArrs.forEach((domain) => {
+    const items = [];
+    for (let i = 0; i < domain.length; i++) {
+      if (items.length === 0) {
+        items.push(`.${domain[i]}`);
+      } else if (domain[i]) {
+        items.push(`${items[items.length - 1]}.${domain[i]}`);
+      }
+    }
+    items.forEach((item) => {
+      if (!result[item]) {
+        result[item] = 1;
+      } else {
+        result[item]++;
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = getDNSStats;
